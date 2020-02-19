e0e25c3ef3222b05fb1f57c433729f1894a98dea
Dmitrii Zavodnov <dmitrii.zavodnov@mail.ru>
2020-02-19 12:34:33 +0300

    Initial Commit

---
+++ Observer (Наблюдатель)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Observer (Наблюдатель)
+//
+//  Created by Дмитрий Заводнов on 2/19/20.
+//  Copyright © 2020 Дмитрий Заводнов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
