dfdfe4982a6e7508d1beecb522bd32d672d74569
Dmitrii Zavodnov <dmitrii.zavodnov@mail.ru>
2020-02-19 13:45:25 +0300

    create observer

--- Observer (Наблюдатель)/ViewController.swift
+++ Observer (Наблюдатель)/ViewController.swift
@@ -1,20 +1,78 @@
-//
-//  ViewController.swift
-//  Observer (Наблюдатель)
-//
-//  Created by Дмитрий Заводнов on 2/19/20.
-//  Copyright © 2020 Дмитрий Заводнов. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textLabel: UILabel!
+    let notificationCenters = NotificationCenters()
+    let conсreteObserver = ConсreteObserver()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    @IBAction func updateActionButton(_ sender: Any) {
+        notificationCenters.someBusinessLogic()
+    }
+    
+    @IBAction func subscribeSwitch(_ sender: UISwitch) {
+        if sender.isOn {
+            notificationCenters.subscribe(self)
+            notificationCenters.subscribe(conсreteObserver)
+        } else {
+            notificationCenters.unSubscribe(self)
+            notificationCenters.unSubscribe(conсreteObserver)
+        }
+    }
 }
 
+//MARK: - ObserverProtocol
+protocol ObserverProtocol: class {
+    func update(subject: NotificationCenters)
+}
+
+//MARK: - NotificationCenters
+class NotificationCenters {
+    var  state: Int = {
+        return Int(arc4random_uniform(10))
+    }()
+    private lazy var observers = [ObserverProtocol]()
+    
+    func subscribe(_ observer: ObserverProtocol) {
+        print(#function)
+        observers.append(observer)
+    }
+    
+    func unSubscribe(_ observer: ObserverProtocol) {
+        if let index = observers.firstIndex(where: {$0 === observer}) { //удаляем обьект по ссылке
+            observers.remove(at: index)
+            print(#function)
+        }
+    }
+    
+    func notify() {
+        print(#function)
+        observers.forEach( { $0.update(subject: self)} )
+    }
+    
+    func someBusinessLogic() {
+        print(#function)
+        state = Int(arc4random_uniform(10))
+        notify()
+    }
+}
+
+//MARK: - ConсreteObserver
+class ConсreteObserver: ObserverProtocol {
+    
+    func update(subject: NotificationCenters) {
+        print("ConсreteObserver - ", subject.state)
+    }
+}
+
+//MARK: - Extension ViewController
+extension ViewController:  ObserverProtocol {
+    
+    func update(subject: NotificationCenters) {
+        textLabel.text = "subject state: - \(subject.state)"
+    }
+}
